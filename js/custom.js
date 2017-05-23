646fec12bb7ee4799fc5287127cf0f305d7b9424
dineshgautam <dineshgautam162@gmail.com>
2017-05-23 15:53:48 +0530

    Test
    Go

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+		$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+});
+
+
+$('ul.nav li.dropdown').hover(function () {
+    $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(200);
+}, function () {
+    $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(200);
+});
+//# sourceURL=pen.js
+
